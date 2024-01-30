e5ededacc5acd4a4bf1f734cd79d6753cb3c0a06
Amitabh Soni <144244954+amitabhsoni2004@users.noreply.github.com>
2024-01-30 22:02:53 +0530

    initial code uploaded

---
+++ script.js
@@ -0,0 +1,23 @@
+let menu = document.querySelector(".menu-bar").addEventListener("click", () => {
+    alert("welcome")
+
+    let content = document.querySelector(".content");
+   //  content.style.backgroundColor = "blue";
+    content.style.display = "block";
+    let menubar = document.querySelector(".menu-bar");
+    menubar.style.display = "none"
+   
+});
+
+
+
+// const hello = document.createElement("div").innerText = "Home";
+   // document.write(hello);
+   // const hell = document.createElement("div").innerText = "Home";
+   // document.write(hello);
+   // const hel = document.createElement("div").innerText = "Home";
+   // document.write(hello);
+   // const he = document.createElement("div").innerText = "Home";
+   // document.write(hello);
+   // const h = document.createElement("div").innerText = "Home";
+   // document.write(hello);
